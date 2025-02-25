6e92445643bcd8e80d203277e7db8d3b6279ffeb
Leonardo Lazilha <leonardolazilha@MacBook-Pro-de-Leonardo.local>
2025-02-25 14:51:23 -0300

    Ajusta CORS para produção

--- src/main/java/com/lawly/controller/PdfController.java
+++ src/main/java/com/lawly/controller/PdfController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/pdf")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class PdfController {
 
     @Autowired
